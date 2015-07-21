ef52c2880e90afb724a56612be2f212490b4b657
Jimil <jimiljojo@gmail.com>
2015-07-21 11:50:09 -0400

    Sorcery authentication complete

---
+++ app/controllers/author_sessions_controller.rb
@@ -0,0 +1,28 @@
+class AuthorSessionsController < ApplicationController
+  include AuthorSessionsHelper
+  def new
+  end
+
+  def create
+    if login(params[:email], params[:password])
+      redirect_back_or_to(articles_path, notice: 'Logged in successfully.')
+    else
+      flash.now.alert = "Login failed."
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:authors, notice: 'Logged out!')
+  end
+
+  before_filter :zero_authors_or_authenticated, only: [:new, :create]
+  before_filter :require_login, except: [:new, :create]
+
+  def zero_authors_or_authenticated
+  	unless Author.count == 0 || current_user
+	  redirect_to root_path
+	  return false
+	end
+end
